076695a6106bee45959950004a09564f46a3b5a4
Hankrezu <hankrezu@gmail.com>
2025-02-07 15:34:19 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import './App.css'
 import NavBar from './components/NavBar'
 import Carousel from './components/Carousel'
 import Popular from './components/Popular'
-import ResVSearch from './components/ResvSearch'
 import OurService from './components/OurService'
 import ClientSaid from './components/ClientSaid'
 import EventUpcoming from './components/Event'
 import Footer from './components/Footer'
+import ResVSearch from './components/ResVSearch'
 
 function App() {
 
